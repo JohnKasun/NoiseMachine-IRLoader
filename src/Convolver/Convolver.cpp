52835ee70eb9e6655b77a8eda0dddcdbb096ce09
John Kasun <johnkasun@live.com>
2022-06-16 15:36:26 -0400

    implement init and reset of convolver

--- src/Convolver/Convolver.cpp
+++ src/Convolver/Convolver.cpp
@@ -2,20 +2,52 @@
 
 Convolver::Convolver()
 {
+	CFft::createInstance(mFft);
 }
 
 Convolver::~Convolver()
 {
+	reset();
+	CFft::destroyInstance(mFft);
 }
 
 Error_t Convolver::init(float* ir, int lengthOfIr, int blockSize)
 {
-	return Error_t();
+	if (!ir)
+		return Error_t::kMemError;
+	if (lengthOfIr < 0 || blockSize < 0)
+		return Error_t::kFunctionInvalidArgsError;
+
+	reset();
+
+	// Copy over Ir
+	mIr.reset(new float[lengthOfIr]);
+	CVectorFloat::copy(mIr.get(), ir, lengthOfIr);
+
+	// Init fft
+	mFft->initInstance(blockSize, 2, CFft::kWindowHann, CFft::kNoWindow);
+	mBlockSize = mFft->getLength(CFft::kLengthData);
+	mFftSize = mFft->getLength(CFft::kLengthFft);
+
+	// Init delay line
+	mDelayLine.reset(new CRingBuffer<float>(mFftSize));
+	mDelayLine->setWriteIdx(mBlockSize);
+
+	// TODO: precompute ir fft 
+	return Error_t::kNoError;
 }
 
 Error_t Convolver::reset()
 {
-	return Error_t();
+	if (mIsInitialized) {
+		mFft->resetInstance();
+		mIr.reset();
+		mDelayLine.reset();
+		mBlockSize = 0;
+		mFftSize = 0;
+		mIsInitialized = false;
+	}
+	return Error_t::kNoError;
 }
 
 Error_t Convolver::process(const float* inputBuffer, float* outputBuffer, int numSamples)
--- src/Convolver/include/Convolver.h
+++ src/Convolver/include/Convolver.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <vector>
+#include <memory>
+
+#include "Vector.h"
+#include "RingBuffer.h"
 #include "ErrorDef.h"
 #include "Fft.h"
-#include <vector>
 
 class Convolver {
 public:
@@ -14,5 +18,13 @@ public:
 
 	Error_t process(const float* inputBuffer, float* outputBuffer, int numSamples);
 private:
+	CFft* mFft = nullptr;;
+	std::unique_ptr<CRingBuffer<float>> mDelayLine = nullptr;
+	std::unique_ptr<float> mIr = nullptr;
 
+	int mBlockSize = 0;
+	int mFftSize = 0;
+	bool mIsInitialized = false;
+
+	// TODO: add buffers for fft computations
 };
